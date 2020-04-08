184ded9595d80cb3b37cc8d6e587e119768fe969
milena.kukolj <mkukolj@protonmail.com>
2020-04-08 16:43:43 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -1,20 +1,67 @@
 class Student {
-	String index;
-	String firstName;
-	String lastName;
-	int grades[];
+    String index;
+    String firstName;
+    String lastName;
+    int grades[];
 
-	//TODO constructor
-    Student() {
-    	
+    Student(String index, String firstName, String lastName, int[] grades) {
+        this.index = index;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.grades = grades;
     }
-	//TODO seters & getters
 
-	public double getAverage() {
-		//TODO
-	}
+    // GETTERS
+    String getIndex() {
+        return this.index;
+    }
 
-	public int ECTSCredits() {
-		//TODO
-	}
+    String getFirstName() {
+        return this.firstName;
+    }
+
+    String getLastName() {
+        return this.lastName;
+    }
+
+    int[] getGrades() {
+        return this.grades;
+    }
+
+    // SETTERS
+    void setIndex(String index) {
+        this.index = index;
+    }
+
+    void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    void setGrades(int[] grades) {
+        this.grades = grades;
+    }
+
+    // METHODS
+    public double getAverage() {
+        int sum = 0;
+
+        for(int i = 0; i < grades.length; i++)
+            sum += grades[i];
+
+        return sum / grades.length;
+    }
+
+    public int ECTSCredits() {
+        int sum = 0;
+
+        for(int i = 0; i < grades.length; i++)
+            if(grades[i] > 5)
+                sum += 6;
+
+        return sum;
+    }
 }
