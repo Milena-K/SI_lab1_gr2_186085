476b0a051980cc70fd696c156a4b656a6b3453d1
milena.kukolj <mkukolj@protonmail.com>
2020-04-08 12:50:41 +0200

    grades list added

--- Student.java
+++ Student.java
@@ -2,9 +2,12 @@ class Student {
 	String index;
 	String firstName;
 	String lastName;
+	int grades[];
 
 	//TODO constructor
-
+    Student() {
+    	
+    }
 	//TODO seters & getters
 
 	public double getAverage() {
